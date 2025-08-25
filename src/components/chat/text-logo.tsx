8925aafe3cdf1bf42e7f1e225c14a1599081f6ae
Amarjeet Yadav <amar@Amarjeets-MacBook-Pro.local>
2025-08-25 12:53:21 +0530

    feature: keeping only out folder

--- src/components/chat/text-logo.tsx
+++
@@ -1,7 +0,0 @@
-'use client";'
-import Link from "next/link";
-export default function TextLogo() {
-    return <h4 className="sm:text-5xl text-4xl :text-sm font-bold tracking-tight mb-6 mt-4 text-left">
-        <Link href={'/'}> doubt<span className="text-indigo-600">Zero</span></Link>
-    </h4>
-}
